e81d3200467b40a68d20e3078b43dd722e05a6d2
root <root@AY130318102739716df4.(none)>
2013-03-18 16:49:56 +0800

    demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qrstore::Application.config.secret_token = '94ed000ee8e268e11b7f953e7ec1bc1f0badb36f4249e07f23d8b53d929543b7dc2fa3f7829a0c00990c30aa6ef008203f5e5bbc6c42e64e56628e52a49d0c06'
